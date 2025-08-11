236a40e239fa36c180a5b9022f6dbd2bb382c5c2
Sifi <sifiro@outlook.com>
2025-08-11 18:13:43 +0200

    Updated Fashion List

--- data/awesomeList/jfashionAwesome.ts
+++ data/awesomeList/jfashionAwesome.ts
@@ -1,14 +1,43 @@
 import {CatType, type Base, type RegularCat, type SuperCat} from "../typeDatas";
 
+const inspiration: RegularCat = {
+    name: "Inspiration / Misc",
+    lists: [
+        {
+            name: "Style Arena",
+            url: "https://www.style-arena.jp/",
+            img: "",
+            description: "Fashion Report Japonesa",
+            category: [""]
+        },
+        {
+            name: "Tokyo Fashion",
+            url: "https://tokyofashion.com/",
+            img: "",
+            description: "Fashion Report Japonesa",
+            category: [""]
+        }
+    ],
+    type: CatType.Cat
+};
+
 const shops: RegularCat = {
     name: "Shops",
-    lists: [{
-        name: "Lolita Wardrobe",
-        url: "https://lolitawardrobe.com/",
-        img: "",
-        description: "Tienda de Lolita Fashion que admite IOSS",
-        category: ["egl"]
-    }
+    lists: [
+        {
+            name: "Lolita Wardrobe",
+            url: "https://lolitawardrobe.com/",
+            img: "",
+            description: "Tienda de Lolita Fashion",
+            category: ["egl","ioss"]
+        },
+        {
+            name: "Madame Chocolat",
+            url: "https://www.madamechocolat-shop.com/",
+            img: "",
+            description: "Tienda de Alt Fashion en la Peninsula.",
+            category: ["alt"]
+        }
     ],
     type: CatType.Cat
 };
@@ -31,40 +60,75 @@ const brands: RegularCat = {
     name: "Brands",
     lists: [
         {
+            name: "Koit Foot Wear",
+            category: ["egl", "goth", "ioss"],
+            url: "https://www.koifootwear.com/",
+            img: "",
+            description: "Tienda Britanica de zapatos alt",
+        },
+        {
             name: "United Tokyo",
-            category: ["Regular"],
-            url: "",
+            category: ["regular"],
+            url: "https://united-tokyo.com/",
             img: "",
             description: "",
         },
         {
             name: "Converse Tokyo",
-            category: ["Regular"],
-            url: "",
+            category: ["regular"],
+            url: "https://converse-tokyo.jp/",
             img: "",
             description: "",
         },
         {
             name: "ListenFlavor",
             category: ["alt"],
-            url: "",
+            url: "https://listenflavor.com/",
             img: "",
             description: "",
         },
         {
             name: "RoyalPrincessAlice",
             category: ["egl"],
-            url: "",
+            url: "https://royalprincessalice.net/",
             img: "",
             description: ""
         },
         {
             name: "OZZON JAPAN",
             category: ["egl"],
-            url: "",
+            url: "https://ozzon-japan.jp/",
             img: "",
             description: ""
         },
+        {
+            name: "NO.S PROJECT",
+            category: ["egl"],
+            url: "https://nos-project.jp/",
+            img: "",
+            description: ""
+        },
+        {
+            name: "Maiden's Rêve❤",
+            category: ["egl"],
+            url: "https://maidensreve.kawaiishop.jp/",
+            img: "",
+            description: ""
+        },
+        {
+            name: "MAYLA",
+            category: ["shoes"],
+            url: "https://lit.link/en/dollsrecruit",
+            img: "",
+            description: ""
+        },
+        {
+            name: "Epetice",
+            category: ["egl"],
+            url: "https://epetice.com/",
+            img: "",
+            description: ""
+        }
     ],
     type: CatType.Cat
 };
@@ -72,6 +136,13 @@ const brands: RegularCat = {
 const idolMasterSpecific: RegularCat = {
     name: "idolm@ster Specific Outfits",
     lists: [
+        {
+            name: "Cinderella Museum",
+            category: [""],
+            url: "https://cinderellamuseum.wixsite.com/cinderella-museum",
+            img: "",
+            description: "Evento/Exhibicion de Cosmakers de deresute."
+        },
         {
             name: "トップ!クローバー",
             url: "",
@@ -100,6 +171,7 @@ const idolMasterSpecific: RegularCat = {
 export const jfashionList: SuperCat = {
     name: "JFashion List",
     lists: [
+        inspiration,
         brands,
         shops,
         styles,
