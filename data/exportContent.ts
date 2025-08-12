436870e0a47d0999be401151a926fc7f898df0ce
Sifi <sifiro@outlook.com>
2025-08-12 16:06:46 +0200

    Implemented Guitars and Events to Export.

--- data/exportContent.ts
+++ data/exportContent.ts
@@ -4,9 +4,12 @@ import {cosplayResources} from "./awesomeList/cosplayResources";
 import {discordCommunities} from "./awesomeList/discordCommunities";
 import {eventCommunities} from "./awesomeList/eventsCommunites";
 import {jfashionList} from "./awesomeList/jfashionAwesome";
+import {guitarResources} from "./awesomeList/musicResources";
 import {youtuber} from "./awesomeList/ytubers";
 import {cosplay} from "./personalList/cosplays";
 import type {cosplayType} from "./personalList/cosplays";
+import {events, type eventType} from "./personalList/eventsStory";
+import {guitars, type Guitar} from "./personalList/guitars";
 import type {RegularCat, SuperCat} from "./typeDatas";
 
 export const awesomeLists: ReadonlyArray<SuperCat | RegularCat> = [
@@ -16,14 +19,19 @@ export const awesomeLists: ReadonlyArray<SuperCat | RegularCat> = [
     communities,
     cosplayResources,
     artistMerch,
-    youtuber
+    youtuber,
+    guitarResources
 ]
 
 export interface MyStuffType {
     cosplay: Array<cosplayType>;
+    guitars: Array<Guitar>;
+    events: Array<eventType>;
 }
 
 export const personalStuff: MyStuffType =
 {
-        cosplay: cosplay
+    cosplay: cosplay,
+    guitars: guitars,
+    events: events,
 }
